82fce19d1360f15271955898fd85e02a010edc2c
krakeshnambiar <43789903+krakeshnambiar@users.noreply.github.com>
2021-02-09 23:18:08 +0530

    Update index.js
    change name 111

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = (event, context, callback) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello All, Welcome to RAK !!')
+        body: JSON.stringify('Hello All, Welcome to RAK111 !!')
     };
     callback(null, response);
 };
