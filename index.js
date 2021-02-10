4ae81b442f1ad4677e30a7516164511931929139
krakeshnambiar <43789903+krakeshnambiar@users.noreply.github.com>
2021-02-10 18:08:20 +0530

    Update index.js
    Demo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = (event, context, callback) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello All, Welcome to RAK111 !!')
+        body: JSON.stringify('Hello All, Welcome to RAK Demo !!')
     };
     callback(null, response);
 };
